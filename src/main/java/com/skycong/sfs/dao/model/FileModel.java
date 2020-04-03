efcb72ee82af6c13718460b4e28ff84f99c0b8b2
randomkiki <wq123457>
2020-04-03 16:43:58 +0800

    add file manager

--- src/main/java/com/skycong/sfs/dao/model/FileModel.java
+++ src/main/java/com/skycong/sfs/dao/model/FileModel.java
@@ -1,11 +1,10 @@
 package com.skycong.sfs.dao.model;
 
-import lombok.Data;
-import lombok.ToString;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * @author ruanmingcong 2019.12.3 11:27
@@ -14,10 +13,10 @@ import javax.persistence.*;
 @Table(name = "tb_simple_web_file")
 @DynamicInsert
 @DynamicUpdate
-@Data
-@ToString
-public class FileModel {
+public class FileModel implements Serializable {
 
+
+    private static final long serialVersionUID = -6187444298403648815L;
     @Id
     @GeneratedValue
     private Integer id;
@@ -45,4 +44,74 @@ public class FileModel {
     private String path;
 
     private Long length;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Long getLength() {
+        return length;
+    }
+
+    public void setLength(Long length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("FileModel{");
+        sb.append("id=").append(id);
+        sb.append(", filename='").append(filename).append('\'');
+        sb.append(", type='").append(type).append('\'');
+        sb.append(", md5='").append(md5).append('\'');
+        sb.append(", url='").append(url).append('\'');
+        sb.append(", path='").append(path).append('\'');
+        sb.append(", length=").append(length);
+        sb.append('}');
+        return sb.toString();
+    }
 }
